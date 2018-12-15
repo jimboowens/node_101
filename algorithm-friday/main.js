1b1f4ca2488bbd4ec405330b88a0efba25f03e30
Jim Owens <jimcowens@gmail.com>
2018-12-15 10:32:21 -0500

    made all functional

--- algorithm-friday/main.js
+++ algorithm-friday/main.js
@@ -1,74 +1,74 @@
 
-// let size = 20
-// size+=1
-// let array = []
+let size = 20
+size+=1
+let array = []
 
 
-// // function to create board and player arrays based on grid size!
-// function makeArray(size) {
-//     let matrix = []
-//     // to populate the top array with 1's
+// function to create board and player arrays based on grid size!
+function makeArray(size) {
+    let matrix = []
+    // to populate the top array with 1's
 
-//     // for the remaining spots
-//     for (i = 0; i < size; i++) {
-//         let row = [1]
-//         array.push(row)
-//         for (j=1;j<size;j++){
-//             array[i].push(1)
-//         }
+    // for the remaining spots
+    for (i = 0; i < size; i++) {
+        let row = [1]
+        array.push(row)
+        for (j=1;j<size;j++){
+            array[i].push(1)
+        }
         
-//     }
-//     // console.log(array)
-//     return array
-// }
-// makeArray(size)
-// // console.log(array)
-// function createBoard(size) {
-//     for (let i = 1; i < size; i++) {
-//         let row = []
-//         for (let j = 1; j < size; j++) {
-//             let rowInRow=[1]
-//             if(array[i-1]){
-//                 if(array[i][j-1]){
-//                     array[i][j]= array[i-1][j]+array[i][j-1]
-//                 } else {array[i][j-1] = 1}
-//             } else{array[i-1]=1} 
-//         }
-//         // console.log(`the solution for ${size-1} is ${array[size-1][size-1]}`)
-//     }
-//     console.log(`the solution for ${size-1} is ${array[size-1][size-1]}`)
-// }
-// createBoard(size)
-// console.log(`the solution for ${size-1} is ${array[size-1][size-1]}`)
-// array.forEach(row => console.log(row));
+    }
+    // console.log(array)
+    return array
+}
+makeArray(size)
+// console.log(array)
+function createBoard(size) {
+    for (let i = 1; i < size; i++) {
+        let row = []
+        for (let j = 1; j < size; j++) {
+            let rowInRow=[1]
+            if(array[i-1]){
+                if(array[i][j-1]){
+                    array[i][j]= array[i-1][j]+array[i][j-1]
+                } else {array[i][j-1] = 1}
+            } else{array[i-1]=1} 
+        }
+        // console.log(`the solution for ${size-1} is ${array[size-1][size-1]}`)
+    }
+    console.log(`the solution for ${size-1} is ${array[size-1][size-1]}`)
+}
+createBoard(size)
+console.log(`the solution for ${size-1} is ${array[size-1][size-1]}`)
+array.forEach(row => console.log(row));
 
-// one super elegant solution that's basically the N!/R!(N-R)!
-// const grid = 20;
-// var path = 1;
+one super elegant solution that's basically the N!/R!(N-R)!
+const grid = 20;
+var path = 1;
 
-// for (i = 0; i < grid; i++) {
-//   path *= (2* grid) - i;
-//   path /= i + 1;
-// }
+for (i = 0; i < grid; i++) {
+  path *= (2* grid) - i;
+  path /= i + 1;
+}
 
-// console.log(path)
+console.log(path)
 
-// one basically like mine, but much more elegant
-// let numRows = 20;
-// let numCols = 20;
-// let matrix = [];
-// for (let row = 0; row < numRows; row++){
-//   matrix.push([]);
-//   for(let cell = 0; cell < numCols; cell++){
-//     if (row === 0 || cell === 0){
-//       matrix[row][cell] = 1;
-//     } else {
-//       matrix[row][cell] = matrix[row-1][cell] + matrix[row][cell-1];
-//     }
-//   }
-// }
-// matrix.forEach(row => console.log(row));
-// console.log(`The number of paths is ${matrix[numRows-1][numCols-1]}`);
+one basically like mine, but much more elegant
+let numRows = 20;
+let numCols = 20;
+let matrix = [];
+for (let row = 0; row < numRows; row++){
+  matrix.push([]);
+  for(let cell = 0; cell < numCols; cell++){
+    if (row === 0 || cell === 0){
+      matrix[row][cell] = 1;
+    } else {
+      matrix[row][cell] = matrix[row-1][cell] + matrix[row][cell-1];
+    }
+  }
+}
+matrix.forEach(row => console.log(row));
+console.log(`The number of paths is ${matrix[numRows-1][numCols-1]}`);
 
 let highestPalendrome = 0
 let factor1
